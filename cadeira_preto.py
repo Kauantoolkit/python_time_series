c013eef5dc9b261699743580f7ff2616313b4c6e
Kauan Cristian <107585090+Kauantoolkit@users.noreply.github.com>
2025-08-25 20:28:23 -0300

    Update cadeira_preto.py

--- cadeira_preto.py
+++ cadeira_preto.py
@@ -1,1 +1,15 @@
 #stuff
+
+#oq é um cabeçalho dentro de um arquivo python, pra q serve, qual a diferença do cabeçalho para a docstring
+
+# o cabeçalho
+
+# Ele serve como um cartão de visitas para o seu código, dando uma visão geral rápida e essencial para quem o ler.
+# um comentário que tem infos como, nome de quem criou, data de edição descrição, nome de licensa e etc.
+# o cabeçalho fica no geral no topo do codigo e não está necessariamente vinculado a uma função.
+
+# docstring
+
+# a docstring é uma especie de explicação sobre a função na qual ela se encontra, o escopo dela e explicar essa função e é possivel acessar até mesmo
+#em tempo de execução com "nomedafuncao.doc"
+
